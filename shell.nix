ae61e9f73e6ec9200ba9b86e61cd72a5a6954893
D. Sunshine <david@sunshines.org>
2025-05-30 22:50:27 -0400

    Basic files
    1. Updated stack support from template
    2. cabal file
    3. Color.hs

---
+++ shell.nix
@@ -0,0 +1,12 @@
+with (import <nixpkgs> { });
+
+stdenv.mkDerivation {
+
+  name = "stack-neovim-lsp";
+
+  buildInputs = [ glpk pcre haskell.packages.lts-3_13.ghc ];
+
+  STACK_IN_NIX_EXTRA_ARGS = " --extra-lib-dirs=${glpk}/lib"
+    + " --extra-include-dirs=${glpk}/include" + " --extra-lib-dirs=${pcre}/lib"
+    + " --extra-include-dirs=${pcre}/include";
+}
